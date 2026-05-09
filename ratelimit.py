d9ef0dd62a427c2310e8673ab70841135f05e636
garden-yardripper <gardenyardripper@gmail.com>
2026-05-09 14:20:34 -0400

    Replace ratelimit middleware with modularized functions

---
+++ ratelimit.py
@@ -0,0 +1,27 @@
+from redis.asyncio import Redis
+from config import settings
+from typing import cast
+from responses import RateLimitedError
+
+async def _check_rate_limit(client: Redis, key: str, rate: int | None = None, per: int | None = None):
+    current = cast(int, await client.incrby(key))
+    
+    if current == 1:
+        await client.expire(key, per or settings.per_seconds)
+    expiration = cast(float, await client.pttl(key)) / 1000
+    
+    if current > (rate or settings.rate_limit):
+        raise RateLimitedError(message="Rate limit exceeded", retry_after=expiration)
+    return settings.rate_limit - current, expiration
+
+async def check_key_rate_limit(client: Redis, api_key: str, rate: int | None = None, per: int | None = None):
+    """Raises `RateLimitedError` (`HTTPException`) if `api_key` is exceeding rate limits - 
+    setting `rate` or `per` overrides settings."""
+    key = f"key_rate_limit:{api_key}"
+    return await _check_rate_limit(client, key, rate, per)
+    
+async def check_ip_rate_limit(client: Redis, ip: str, rate: int | None = None, per: int | None = None):
+    """Raises `RateLimitedError` (`HTTPException`) if `ip` is exceeding rate limits - 
+    setting `rate` or `per` overrides settings."""
+    key = f"ip_rate_limit:{ip}"
+    return await _check_rate_limit(client, key, rate, per)
